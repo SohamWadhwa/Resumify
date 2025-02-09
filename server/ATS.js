2aa4f928068d9cd92065bec62a5ee1547f3892d3
SohamWadhwa <soham.wadhwa0712@gmail.com>
2025-02-09 20:30:27 +0530

    Commit no.2

--- server/ATS.js
+++ server/ATS.js
@@ -9,11 +9,12 @@ async function generateATS(resume) {
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
     
-    const prompt = ` MUST Return Integer, provide ats score of the given resume using these keywords: ${keywords}
+    const prompt = ` MUST Return Integer u dumb AI, provide ats score of the given resume by considering these keywords: ${keywords}
     Resume: ${resume}`;
     
     const result = await model.generateContent(prompt);
     const score = result.response?.text() || "Error";
+    
     return score;
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Tips from './pages/Tips';
 import Aboutme from './pages/About';
@@ -10,11 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-        </Routes>
-        <Routes>
           <Route path="/tips" element={<Tips />} />
-        </Routes>
-        <Routes>
           <Route path='/about-me' element={<Aboutme />} />
         </Routes>
       </Router>
